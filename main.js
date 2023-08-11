7aec9c1b22f6ad049cb263d29fad59a38d2c32c6
Nguyễn Tiến Mạnh <84314350+fairplay012@users.noreply.github.com>
2023-08-11 22:02:06 +0700

    done

--- main.js
+++
@@ -1,7 +0,0 @@
-const serviceTag = document.getElementById('service-scroll');
-const targetService = document.getElementsByClassName('service')[0];
-
-serviceTag.addEventListener('click', () => {
-    targetService.scrollIntoView({ behavior: 'smooth' });
-});
-
